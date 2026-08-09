e1fa67ba229d7894ecc8322d02dcc851a39d7fe4
Georg Federmann <georg.federmann@gmail.com>
2026-08-09 21:57:06 +0200

    Move options to dedicated lua module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,3 @@
 do
-	-- local message = "local hello!"
-	-- -- -- -- -- -- -- -- -- print(message)
-
-	-- vim.o.showmode = false
-	vim.o.breakindent = true
-	vim.o.clipboard = "unnamedplus"
-	vim.o.confirm = true
-	vim.o.cursorline = true
-	vim.o.hlsearch = true
-	vim.o.ignorecase = true
-	vim.o.list = true
-	vim.o.mouse = "a"
-	vim.o.number = true
-	vim.o.relativenumber = true
-	vim.o.scrolloff = 0
-	vim.o.showmode = true
-	vim.o.signcolumn = "yes"
-	vim.o.smartcase = true
-	vim.o.splitbelow = true
-	vim.o.splitright = true
-	vim.o.timeoutlen = 300
-	vim.o.undofile = true
-	vim.o.updatetime = 250
-	vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+	require("options")
 end
--- print(message)
--- require("options")
---
+++ .config/nvim/lua/options/init.lua
@@ -0,0 +1,22 @@
+do
+	vim.o.breakindent = true
+	vim.o.clipboard = "unnamedplus"
+	vim.o.confirm = true
+	vim.o.cursorline = true
+	vim.o.hlsearch = true
+	vim.o.ignorecase = true
+	vim.o.list = true
+	vim.o.mouse = "a"
+	vim.o.number = true
+	vim.o.relativenumber = true
+	vim.o.scrolloff = 0
+	vim.o.showmode = true
+	vim.o.signcolumn = "yes"
+	vim.o.smartcase = true
+	vim.o.splitbelow = true
+	vim.o.splitright = true
+	vim.o.timeoutlen = 300
+	vim.o.undofile = true
+	vim.o.updatetime = 250
+	vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+end
