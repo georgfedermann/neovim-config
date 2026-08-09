d2e68a11e5a278cc4ed49ad5d7b8d15756fa1350
Georg Federmann <georg.federmann@gmail.com>
2026-08-09 18:31:36 +0200

    Move from vim.o to vim.opt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,11 @@
 do
-  local message = "local hello!"
-  print(message)
+	local message = "local hello!"
+	print(message)
 
-  vim.o.number = true
-  vim.o.relativenumber = true
+	-- vim.opt.number = true
+	vim.opt.relativenumber = true
+	vim.opt.showmode = false
+	vim.opt.clipboard = "unnamedplus"
 end
 print(message)
 require("options")
