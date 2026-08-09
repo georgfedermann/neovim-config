9a199dce91afed87bd1bb30ecb7a7c05d80ebb46
Georg Federmann <georg.federmann@gmail.com>
2026-08-09 21:48:57 +0200

    - Add a longer markdown file to test features like scrolling, etc. - Clean up initial vim.o configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,28 @@
 do
-	local message = "local hello!"
-	print(message)
+	-- local message = "local hello!"
+	-- -- -- -- -- -- -- -- -- print(message)
 
-	-- vim.opt.number = true
-	vim.opt.relativenumber = true
-	vim.opt.showmode = false
-	vim.opt.clipboard = "unnamedplus"
+	-- vim.o.showmode = false
+	vim.o.breakindent = true
+	vim.o.clipboard = "unnamedplus"
+	vim.o.confirm = true
+	vim.o.cursorline = true
+	vim.o.hlsearch = true
+	vim.o.ignorecase = true
+	vim.o.list = true
+	vim.o.mouse = "a"
+	vim.o.number = true
+	vim.o.relativenumber = true
+	vim.o.scrolloff = 0
+	vim.o.showmode = true
+	vim.o.signcolumn = "yes"
+	vim.o.smartcase = true
+	vim.o.splitbelow = true
+	vim.o.splitright = true
+	vim.o.timeoutlen = 300
+	vim.o.undofile = true
+	vim.o.updatetime = 250
+	vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 end
-print(message)
-require("options")
+-- print(message)
+-- require("options")
