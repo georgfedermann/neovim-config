22fe2d5c3c8edcd63a8e4495ac5c633529762767
Georg Federmann <georg.federmann@gmail.com>
2026-08-14 21:46:37 +0200

    - Move keymappings to .config/nvim/lua/keymaps/init.lua - Add grin keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,4 @@ do
 	require("globals")
 	require("options")
 	require("keymaps")
-
-	vim.keymap.set("i", "<C-s>", "<cmd>write<CR>", { desc = "Write file" })
-	vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Write file" })
 end
--- .config/nvim/lua/keymaps/init.lua
+++ .config/nvim/lua/keymaps/init.lua
@@ -1,7 +1,7 @@
 do
 	vim.keymap.set("i", "<C-s>", "<cmd>write<CR>", { desc = "Write file in insert mode" })
 	vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Write file in normal mode" })
-	vim.keymap.set("i", "<C-g>", function()
+	vim.keymap.set("n", "<leader>g", function()
 		print(":-> \\o/ yay!")
 	end)
 end
